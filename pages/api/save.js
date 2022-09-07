f21a8f5a2724a704d5a0710ba2028504c8f2678a
tlodge <tlodge@gmail.com>
2022-09-07 12:46:27 +0100

    test commit

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,13 +1,16 @@
 import firebase from '../../lib/firebase';
 
 export default async function handler(req, res) {
+
+    console.log("incoming", req.method);
+
     if (req.method !== 'POST') {
-        res.status(405).send({ message: 'Only POST requests allowed' })
-        return
+        //res.status(405).send({ message: 'Only POST requests allowed' })
+        ///return
     }
     const {id, script} = req.body;
+    console.log("seen post", id, script);
     if (id && script){
-        console.log("seen post", id, script);
         const docRef = firebase.collection("scripts").doc(id);
         await docRef.set({...script, ts:Date.now()});
         res.status(200).json({ success: true})
