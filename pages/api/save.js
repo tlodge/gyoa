23151259057d7c127a33efdb993e74a77e56a5e3
tlodge <tlodge@gmail.com>
2022-09-07 12:23:14 +0100

    fireabase api

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,7 +1,8 @@
+import firebase from '../../lib/firebase';
+
 export default async function handler(req, res) {
-    const pdata = req.body;
-    const gdata = req.query;
-    console.log(pdata);
-    console.log(gdata);
-    res.status(200).json({ hello: 'person' })
+    const {id, script} = req.body;
+    const docRef = firebase.collection("scripts").doc(id);
+    await docRef.set({...script, ts:Date.now()});
+    res.status(200).json({ success: true})
 }
