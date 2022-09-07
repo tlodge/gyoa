06a7bff63f3ccdfe0140c5048a61474a5905d060
tlodge <tlodge@gmail.com>
2022-09-07 12:40:49 +0100

    improved endpoint

--- pages/api/save.js
+++ pages/api/save.js
@@ -1,9 +1,17 @@
 import firebase from '../../lib/firebase';
 
 export default async function handler(req, res) {
-    const {id="tempid", script} = req.body;
-    console.log("seen post", id, script);
-    const docRef = firebase.collection("scripts").doc(id);
-    await docRef.set({...script, ts:Date.now()});
-    res.status(200).json({ success: true})
+    if (req.method !== 'POST') {
+        res.status(405).send({ message: 'Only POST requests allowed' })
+        return
+    }
+    const {id, script} = req.body;
+    if (id && script){
+        console.log("seen post", id, script);
+        const docRef = firebase.collection("scripts").doc(id);
+        await docRef.set({...script, ts:Date.now()});
+        res.status(200).json({ success: true})
+    }else{
+        res.status(405).send({message: "Malformed request"})
+    }
 }
